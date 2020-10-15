bd04c0d706e53b5765c7d845aeac224f8cd43902
Kamil Salawa <emir.salawa@gmail.com>
2020-10-15 15:32:11 +0100

    final

--- calculator.js
+++ calculator.js
@@ -19,16 +19,63 @@ const ingredient = (name, weight, kcal, fat, carb, protein) => {
     
 };
 
-const olives = ingredient('olives', 300, 162,0,1);
-const pesto = ingredient('pesto', 312, 27.3, 10.6, 4.8);
-const feta = ingredient('feta', 303, 26.1, 0.7, 16.2);
-const cream = ingredient('cream', 467, 50.5, 1.6, 1.5);
-const butter = ingredient('butter', 745, 82, 0.6, 0.6);
-const chicken = ingredient('chicken', 196, 10.9, 0, 24.4);
+const olives = ingredient('olives', 300, 162, 17, 0, 1);
+const pesto = ingredient('pesto', 190, 312, 27.3, 10.6, 4.8);
+const feta = ingredient('feta', 400, 303, 26.1, 0.7, 16.2);
+const cream = ingredient('cream', 600, 467, 50.5, 1.6, 1.5);
+const butter = ingredient('butter', 250, 745, 82, 0.6, 0.6);
+const chicken = ingredient('chicken', 1200, 196, 10.9, 0, 24.4);
+const allIngredients = [olives, pesto, feta, cream, butter, chicken]
 
-console.log(olives, pesto, feta, cream, butter, chicken);
+const kcal = 0;
+const fat = 0;
+const carb = 0;
+const protein = 0;
 
-const caloriesPerNutrient = () => {
 
+    
+const fatKcal = [];
+
+for (i = 0; i < allIngredients.length; i++) {
+    fatKcal.push(allIngredients[i].fat * 9 * (allIngredients[i].weight / 100));
 };
 
+const carbKcal = [];
+
+for (i = 0; i < allIngredients.length; i++) {
+    carbKcal.push(allIngredients[i].carb * 4 * (allIngredients[i].weight / 100));
+};
+
+const proteinKcal = [];
+
+for (i = 0; i < allIngredients.length; i++) {
+    proteinKcal.push(allIngredients[i].protein * 4 * (allIngredients[i].weight / 100));
+};
+
+
+
+//console.log(fatTotal);
+
+//add up fat array
+const totalFat = fatKcal.reduce(function(a, b){
+    return Math.round(a+b);
+}, 0);
+const totalCarb = carbKcal.reduce(function(a, b){
+    return Math.round(a+b);
+}, 0);
+const totalProtein = proteinKcal.reduce(function(a, b){
+    return Math.round(a+b);
+}, 0);
+
+console.log(`Today's ingredients:
+`)
+
+for (i = 0; i < allIngredients.length; i++) {
+    console.log(allIngredients[i].name);
+};
+
+console.log(`
+Total calories from fat ${totalFat}.`);
+console.log(`Total calories from carb ${totalCarb}.`);
+console.log(`Total calories from protein ${totalProtein}.`);
+console.log(`Calories in whole meal : ${totalFat+totalCarb+totalProtein}, calories in one portion: ${Math.round((totalFat+totalCarb+totalProtein) / 7)}.`)
